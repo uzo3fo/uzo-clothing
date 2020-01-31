fbb3810e52244c1cb6d01799f666a70c85f48cb8
fredrick osuala <fredosuala@gmail.com>
2020-01-31 08:59:55 +0100

    update menu-items to navigate using react-router-dom/withRouter()

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.css';
-
+import{ Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import HatsPage from './pages/HatsPage'
 
 
 
 function App() {
   return (
     <div>
-      <HomePage />
+     <Switch>
+        <Route path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+     </Switch>
+     }
     </div>
   );
 }
---
+++ src/pages/HatsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HatsPage() {
+    return (
+        <div>
+            <h1>HATS-PAGE</h1>
+        </div>
+    )
+}
+
+export default HatsPage
