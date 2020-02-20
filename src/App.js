9f363b43cd4c27abaa924acc06d8cfd69e27dd3f
fredrick osuala <fredosuala@gmail.com>
2020-02-20 10:16:35 +0100

    updating app to redirect to home if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import{ Route, Switch } from 'react-router-dom';
+import{ Route, Switch, Redirect } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shoppage.component';
 import Header from './components/header/Header'
@@ -49,17 +49,28 @@ class App extends React.Component{
         <Header />
        <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route exact path='/shop' component={ShopPage} />
-          <Route exact path='/signin' component={Sign} />
+          <Route  path='/shop' component={ShopPage} />
+          <Route exact 
+            path='/signin' 
+            render = { ()=> 
+            this.props.currentUser ? 
+            (<Redirect to='/'  />) :
+            (<Sign />)
+            }
+          />
        </Switch>
       </div>
     );
   }
   
 }
+//redirect
+const mapStateToProps = ({ user }) =>({
+  currentUser: user.currentUser
+})
 
 const mapDispatchToProps = dispatch =>({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,29 +1,40 @@
 import React from 'react';
-import './header.styles.scss'
-import { Link } from 'react-router-dom'
-import { auth } from '../../firebase/firebase.utils'
-import { connect } from 'react-redux'
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-function Header({ currentUser }) {
-    return (
-        <div className='header'>
-            <Link to='/' className="logo-container">UZO</Link>
-            <div className="options">
-                <Link className="option" to='/shop'>SHOP</Link>
-                <Link className="option" to='/contact'>CONTACT</Link>
-                {
-                    currentUser?
-                    <div className='option' onClick={()=>auth.signOut()}>SIGN OUT</div>
-                    :
-                    <Link className='option' to='/signin'>SIGN IN</Link>
-                }
-            </div>
+import { auth } from '../../firebase/firebase.utils';
+
+//import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+import './header.styles.scss';
+
+const Header = ({ currentUser }) => (
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      UZO
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
+      </Link>
+      <Link className='option' to='/shop'>
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
+          SIGN OUT
         </div>
-    )
-}
+      ) : (
+        <Link className='option' to='/signin'>
+          SIGN IN
+        </Link>
+      )}
+    </div>
+  </div>
+);
 
-const mapStateToProps = state =>({
-    currentUser: state.currentUser
-})
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
 
-export default connect (mapStateToProps)(Header)
+export default connect(mapStateToProps)(Header);
